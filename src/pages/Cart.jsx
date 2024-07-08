7eb1627aee8e926ffcd3e384f79ff7a9ea20153c
dollarp-code <husseindollarp@yahoo.com>
2024-07-08 17:43:05 +0100

    Checkout

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,76 +8,78 @@ const Cart = () => {
   const [cartItems, setCartitems] = useState(cart);
   return (
     <>
-      <Header />
-      <section className="cart-container">
-        <div className="cart-container-left">
-          <div className="shopping">
-            <h3>Shopping Cart</h3>
-            <Link to="/">
-              <h5>Continue Shopping</h5>
-            </Link>
-          </div>
-          <div>
-            {cartItems.map((item) => {
-              const { image, id, title, price, quantity, color } = item;
-              return (
-                <article key={id} className="cart-items-container">
-                  <div className="box">
-                    <img src={image} alt={title} className="image" />
-                  </div>
-                  <div className="cart-desc">
-                    <div className="description">
-                      <h3>{title}</h3>
-                      <div className="color">
-                        <h4>Select Color:</h4>
-                        <div>
-                          <img src={color} alt={title} />
+      <div style={{ background: '#F6F6F5' }}>
+        <Header />
+        <section className="cart-container">
+          <div className="cart-container-left">
+            <div className="shopping">
+              <h3>Shopping Cart</h3>
+              <Link to="/">
+                <h5>Continue Shopping</h5>
+              </Link>
+            </div>
+            <div>
+              {cartItems.map((item) => {
+                const { image, id, title, price, quantity, color } = item;
+                return (
+                  <article key={id} className="cart-items-container">
+                    <div className="box">
+                      <img src={image} alt={title} className="image" />
+                    </div>
+                    <div className="cart-desc">
+                      <div className="description">
+                        <h3>{title}</h3>
+                        <div className="color">
+                          <h4>Select Color:</h4>
+                          <div>
+                            <img src={color} alt={title} />
+                          </div>
+                        </div>
+                        <div className="shuffle-container">
+                          <img src="/images/shuffle.svg" alt="icon" />
+                          <h6>Compare with similar items </h6>
                         </div>
                       </div>
-                      <div className="shuffle-container">
-                        <img src="/images/shuffle.svg" alt="icon" />
-                        <h6>Compare with similar items </h6>
+                      <div>
+                        <h5>{quantity}</h5>
+                        <h6>Remove</h6>
                       </div>
+                      <div className="amount">{price}</div>
                     </div>
-                    <div>
-                      <h5>{quantity}</h5>
-                      <h6>Remove</h6>
-                    </div>
-                    <div className="amount">{price}</div>
-                  </div>
-                </article>
-              );
-            })}
-          </div>
-        </div>
-        <div className="cart-container-right">
-          <div className="order-summary">
-            <h3>Order Summary</h3>
-            <div className="cost-price ">
-              <div className="subtotal">
-                <h5>Subtotal</h5>
-                <h4>$1,120.00</h4>
-              </div>
-              <div className="shipping">
-                <h5>Shipping Fee</h5>
-                <h4>$300.00</h4>
-              </div>
-              <div className="line line-order-summary"></div>
-              <div className="total">
-                <h5>Total</h5>
-                <h4>$1,1420</h4>
-              </div>
+                  </article>
+                );
+              })}
             </div>
-            <Link to="/checkout">
-              <button type="button" className="order-button">
-                Proceed to Checkout
-              </button>
-            </Link>
           </div>
-          <div></div>
-        </div>
-      </section>
-      <Footer />
+          <div className="cart-container-right">
+            <div className="order-summary">
+              <h3>Order Summary</h3>
+              <div className="cost-price ">
+                <div className="subtotal">
+                  <h5>Subtotal</h5>
+                  <h4>$1,120.00</h4>
+                </div>
+                <div className="shipping">
+                  <h5>Shipping Fee</h5>
+                  <h4>$300.00</h4>
+                </div>
+                <div className="line line-order-summary"></div>
+                <div className="total">
+                  <h5>Total</h5>
+                  <h4>$1,1420</h4>
+                </div>
+              </div>
+              <Link to="/checkout">
+                <button type="button" className="order-button">
+                  Proceed to Checkout
+                </button>
+              </Link>
+            </div>
+            <div></div>
+          </div>
+        </section>
+        <Footer />
+      </div>
     </>
   );
 };
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -4,76 +4,164 @@ import { cart } from '../data';
 
 const Checkout = () => {
   return (
-    <section className="checkout">
-      <Header />
-      <section className="checkout-container">
-        <div className="checkout-container-left">
-          <div>
-            {/* <a href="#">Back to cart</a> */}
-            <Link to="/cart">Back to cart</Link>
+    <div style={{ background: '#f6f6f5' }}>
+      <section className="checkout">
+        <Header />
+        <section className="checkout-container">
+          <div className="checkout-container-left">
+            <div className="check-contain">
+              <div style={{ marginBottom: '3rem' }}>
+                {/* <a href="#">Back to cart</a> */}
+                <Link to="/cart">Back to cart</Link>
+              </div>
+              <div style={{ marginBottom: '1rem' }}>
+                <h4>Checkout</h4>
+                <h6>pay for the things you are buying here</h6>
+              </div>
+              <form action="#" className="left-form">
+                <h5 style={{ marginBottom: '2rem' }}>1. Contact Information</h5>
+                <div className="two-column">
+                  <div>
+                    <label htmlFor="name" className="left-label">
+                      First name
+                    </label>
+                    <input type="text" name="name" id="name" />
+                  </div>
+                  <div>
+                    <label htmlFor="lname" className="left-label">
+                      Last name
+                    </label>
+                    <input type="text" name="lname" id="lname" />
+                  </div>
+                </div>
+                <div className="two-column">
+                  <div>
+                    <label htmlFor="number" className="left-label">
+                      Phone Number
+                    </label>
+                    <input type="text" name="number" id="number" />
+                  </div>
+                  <div>
+                    <label htmlFor="email" className="left-label">
+                      Email Address
+                    </label>
+                    <input type="text" name="email" id="email" />
+                  </div>
+                </div>
+
+                <h5>2. Delivery Method</h5>
+                <div className="radio-container">
+                  <div className="radio">
+                    <input type="radio" />
+                    <h6>same-day</h6>
+                  </div>
+                  <div className="radio">
+                    <input type="radio" />
+                    <h6>Express</h6>
+                  </div>
+                  <div className="radio">
+                    <input type="radio" />
+                    <h6>Normal</h6>
+                  </div>
+                </div>
+                <h5>3. Payment Method</h5>
+                <div className="radio-container">
+                  <div className="radio">
+                    <input type="radio" />
+                    <h6>Gpay</h6>
+                  </div>
+                  <div className="radio">
+                    <input type="radio" />
+                    <h6>paypal</h6>
+                  </div>
+                  <div className="radio">
+                    <input type="radio" />
+                    <h6>applepay</h6>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
-          <div>
-            <h4>Checkout</h4>
-            <p>pay for the things you are buying here</p>
-          </div>
-        </div>
-        <div className="checkout-container-right">
-          <div className="checkout-summary">
-            <h3>Payment Information</h3>
-            <form
-              action="
+
+          <div className="checkout-container-right">
+            <div className="checkout-summary">
+              <h3>Payment Information</h3>
+              <form
+                className="right-form"
+                action="
             #"
-            >
-              <div className="two-column">
+              >
+                <div className="two-column">
+                  <div>
+                    <label htmlFor="name" className="right-label">
+                      First Name
+                    </label>
+                    <input
+                      className="right-input"
+                      type="text"
+                      placeholder="Enter your first name"
+                      name="name"
+                      id="name"
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="lastname" className="right-label">
+                      Last Name
+                    </label>
+                    <input
+                      className="right-input"
+                      type="text"
+                      placeholder="Enter your last name"
+                      id="lastname"
+                    />
+                  </div>
+                </div>
                 <div>
-                  <label htmlFor="name">First Name</label>
+                  <label htmlFor="card" className="right-label">
+                    Card Number
+                  </label>
                   <input
-                    type="text"
-                    placeholder="Enter your first name"
-                    name="name"
-                    id="name"
+                    className="right-input"
+                    type="number"
+                    placeholder="Enter card number"
+                    id="card"
+                    name="card"
                   />
                 </div>
-                <div>
-                  <label htmlFor="lastname">Last Name</label>
-                  <input
-                    type="text"
-                    placeholder="Enter your last name"
-                    id="lastname"
-                  />
+                <div className="two-column">
+                  <div>
+                    <label htmlFor="date" className="right-label">
+                      Expiry Date
+                    </label>
+                    <input
+                      className="right-input"
+                      type="text"
+                      placeholder="Enter card expiry date"
+                      name="date"
+                      id="date"
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="cvv" className="right-label">
+                      CVV
+                    </label>
+
+                    <input
+                      type="number"
+                      placeholder="Enter card cvv"
+                      id="cvv"
+                      className="right-input"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div>
-                <label htmlFor="card">Card Number</label>
-                <input
-                  type="number"
-                  placeholder="Enter card number"
-                  id="card"
-                  name="card"
-                />
-              </div>
-              <div className="two-column">
-                <div>
-                  <label htmlFor="date">Expiry Date</label>
-                  <input
-                    type="text"
-                    placeholder="Enter card expiry date"
-                    name="date"
-                    id="date"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="cvv">CVV</label>
-                  <input type="number" placeholder="Enter card cvv" id="cvv" />
-                </div>
-              </div>
-            </form>
-            <button className="order-button">Proceed to Payment</button>
+              </form>
+              <button className="order-button">Proceed to Payment</button>
+            </div>
+            <div></div>
           </div>
-          <div></div>
-        </div>
+        </section>
       </section>
-    </section>
+    </div>
   );
 };
 export default Checkout;
